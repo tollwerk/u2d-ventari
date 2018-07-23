b3269047411ff038189d9e2f9015cf668e8794e8
Philip Saa <philip@tollwerk.de>
2018-07-23 20:20:15 +0200

    coverage svg

--- src/Ventari/Tests/Ports/DemoTest.php
+++ src/Ventari/Tests/Ports/DemoTest.php
@@ -30,5 +30,8 @@ class DemoTest extends AbstractTestBase
         $property2 = md5(rand());
         $demo->setProperty($property2);
         $this->assertEquals($property2, $demo->getProperty());
+
+//        $this->assertEquals([1], [2]);
+        $this->assertJsonStringEqualsJsonFile( dirname(__DIR__).DIRECTORY_SEPARATOR.'Fixture'.DIRECTORY_SEPARATOR.'result.json', '{"foo":"bar"}');
     }
 }
