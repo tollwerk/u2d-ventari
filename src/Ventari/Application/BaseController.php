3ea2fd909184d130a67a699323ccf235207aa8fe
cowglow <cowglow@gmail.com>
2018-07-24 16:22:13 +0200

    Tollwerk Code Styling Update

--- src/Ventari/Application/BaseController.php
+++ src/Ventari/Application/BaseController.php
@@ -2,22 +2,24 @@
 
 namespace Tollwerk\Ventari\Application;
 
-class BaseController {
+class BaseController
+{
 
-	public $project_name;
+    public $project_name;
 
-	public $config_file;
-	public $configuration;
+    public $config_file;
+    public $configuration;
 
-	public function __construct(string $project_name) {
+    public function __construct(string $project_name)
+    {
 //		New Way
-		$this->project_name = ( isset($project_name) ) ? $project_name : "Ventari";
+        $this->project_name = (isset($project_name)) ? $project_name : "Ventari";
 
-		$this->config_file = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '../config.xml';
+        $this->config_file = __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'../config.xml';
 //		$this->configuration = (file_exists($this->config_file)) ? simplexml_load_file($this->config_file) : new \DOMElement('<config/>');
 
-		if ( file_exists( $this->config_file ) ) {
-			$this->configuration = simplexml_load_file( $this->config_file );
-		}
-	}
+        if (file_exists($this->config_file)) {
+            $this->configuration = simplexml_load_file($this->config_file);
+        }
+    }
 }
--- src/Ventari/Tests/Applications/BaseControllerTest.php
+++ src/Ventari/Tests/Applications/BaseControllerTest.php
@@ -10,34 +10,37 @@ use Tollwerk\Ventari\Tests\AbstractTestBase;
  *
  * @package Tollwerk\Ventari\Tests\Application
  */
-class BaseControllerTest extends AbstractTestBase {
+class BaseControllerTest extends AbstractTestBase
+{
 
-	/**
-	 * Test the BaseController
-	 */
-	public function testConstructor() {
-		$baseController = new BaseController( 'Ventari' );
-		$this->assertClassHasAttribute( 'project_name', BaseController::class );
-		$this->assertInstanceOf( BaseController::class, $baseController );
-		$this->assertEquals( 'Ventari', $baseController->project_name );
+    /**
+     * Test the BaseController
+     */
+    public function testConstructor()
+    {
+        $baseController = new BaseController('Ventari');
+        $this->assertClassHasAttribute('project_name', BaseController::class);
+        $this->assertInstanceOf(BaseController::class, $baseController);
+        $this->assertEquals('Ventari', $baseController->project_name);
 
-		return $baseController;
-	}
+        return $baseController;
+    }
 
-	/**
-	 * Test the Configuration XML file
-	 * @depends testConstructor
-	 *
-	 * @param $baseController
-	 */
-	public function testConfiguration( $baseController ) {
-		$this->assertNotEmpty( $baseController );
+    /**
+     * Test the Configuration XML file
+     * @depends testConstructor
+     *
+     * @param $baseController
+     */
+    public function testConfiguration($baseController)
+    {
+        $this->assertNotEmpty($baseController);
 
-		$expectedXML = new \SimpleXMLElement( '<config clientId="U2D Ventari" type="REST"><protocol>http</protocol><uri>events.nueww.de</uri><path>/events</path></config>' );
-		$expectedXML->saveXML();
-		$actualXML = $baseController->configuration;
+        $expectedXML = new \SimpleXMLElement('<config clientId="U2D Ventari" type="REST"><protocol>http</protocol><uri>events.nueww.de</uri><path>/events</path></config>');
+        $expectedXML->saveXML();
+        $actualXML = $baseController->configuration;
 
-		$this->assertFileExists( $baseController->config_file );
-		$this->assertXmlStringEqualsXmlString( $expectedXML->asXML(), $actualXML->asXML() );
-	}
+        $this->assertFileExists($baseController->config_file);
+        $this->assertXmlStringEqualsXmlString($expectedXML->asXML(), $actualXML->asXML());
+    }
 }
