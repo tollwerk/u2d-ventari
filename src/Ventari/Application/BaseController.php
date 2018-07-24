9b24c1b47e0d5b56ce3bebf2721480f6d3d15b39
cowglow <cowglow@gmail.com>
2018-07-24 15:13:54 +0200

    Testing XML Assertion

--- src/Ventari/Application/BaseController.php
+++ src/Ventari/Application/BaseController.php
@@ -6,16 +6,18 @@ class BaseController {
 
 	public $project_name;
 
+	public $config_file;
+	public $configuration;
+
 	public function __construct(string $project_name) {
 //		New Way
 		$this->project_name = (isset($project_name)) ? $project_name : "Ventari";
 
-//		Old Way
-//		if (isset($project_name)){
-//			$this->project_name = $project_name;
-//		} else {
-//			$this->project_name = 'Ventari';
-//		}
-	}
+		$this->config_file = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '../config.xml';
+//		$this->configuration = (file_exists($this->config_file)) ? simplexml_load_file($this->config_file) : new \DOMElement('<config/>');
 
+		if (file_exists($this->config_file)){
+			$this->configuration = simplexml_load_file($this->config_file);
+		}
+	}
 }
