cf30b765c4dd7b49e309623fd8f7f88edbaf8625
cowglow <cowglow@gmail.com>
2018-08-01 10:03:37 +0200

    CC Update 1

--- src/Ventari/Domain/VentariClient.php
+++ src/Ventari/Domain/VentariClient.php
@@ -5,6 +5,10 @@ namespace Tollwerk\Ventari\Domain;
 use Tollwerk\Ventari\Domain\Contract\ControllerInterface;
 use Tollwerk\Ventari\Domain\Contract\HttpClientInterface;
 
+/**
+ * Class VentariClient
+ * @package Tollwerk\Ventari\Domain
+ */
 class VentariClient
 {
     /**
@@ -17,11 +21,15 @@ class VentariClient
      */
     protected $client;
 
+    /**
+     * VentariClient constructor.
+     *
+     * @param ControllerInterface $config
+     * @param HttpClientInterface $client
+     */
     public function __construct(ControllerInterface $config, HttpClientInterface $client)
     {
         $this->config = $config;
         $this->client = $client;
     }
-
-
 }
