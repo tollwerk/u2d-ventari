319e778f44239fb52da824432ce61a137ae04a7b
cowglow <cowglow@gmail.com>
2018-08-01 10:06:00 +0200

    CC Update 2

--- src/Ventari/Domain/Model/AbstractModel.php
+++ src/Ventari/Domain/Model/AbstractModel.php
@@ -16,24 +16,20 @@ abstract class AbstractModel
     /**
      * @return int
      */
-    public function getId(){
+    public function getId()
+    {
         return $this->id;
     }
 
     /**
      * @param $id
+     *
      * @return $this
      */
-    public function setId($id){
+    public function setId($id)
+    {
         $this->id = $id;
+
         return $this;
     }
-
-    /***************************************************************
-     * GET Functions
-     **************************************************************/
-
-    /***************************************************************
-     * SET Functions
-     **************************************************************/
 }
