eca7620a3b4b863d8d09b12ef59e743bcd387651
Philip Saa <philip@tollwerk.de>
2018-08-02 11:04:44 +0200

    Event Traits

--- src/Ventari/Domain/Model/Event.php
+++ src/Ventari/Domain/Model/Event.php
@@ -3,6 +3,7 @@
 namespace Tollwerk\Ventari\Domain\Model;
 
 use Tollwerk\Ventari\Domain\Contract\EventInterface;
+use Tollwerk\Ventari\Domain\Model\Traits\ContactTrait;
 
 /**
  * Class Event
@@ -10,6 +11,11 @@ use Tollwerk\Ventari\Domain\Contract\EventInterface;
  */
 class Event extends AbstractModel implements EventInterface
 {
+    /**
+     * Use traits
+     */
+    use ContactTrait;
+
     /**
      * @var string
      */
@@ -24,41 +30,6 @@ class Event extends AbstractModel implements EventInterface
      * @var string
      */
     protected $eventCity;
-
-    /**
-     * @var string
-     */
-    protected $eventContactEmail;
-
-    /**
-     * @var string
-     */
-    protected $eventContactFacebook;
-
-    /**
-     * @var string
-     */
-    protected $eventContactInstagram;
-
-    /**
-     * @var string
-     */
-    protected $eventContactLinks;
-
-    /**
-     * @var string
-     */
-    protected $eventContactLogo;
-
-    /**
-     * @var string
-     */
-    protected $eventContactName;
-
-    /**
-     * @var string
-     */
-    protected $eventContactTwitterHandle;
 
     /**
      * @var string
@@ -236,118 +207,6 @@ class Event extends AbstractModel implements EventInterface
     public function setEventCity(string $eventCity): void
     {
         $this->eventCity = $eventCity;
-    }
-
-    /**
-     * @return string
-     */
-    public function getEventContactEmail(): string
-    {
-        return $this->eventContactEmail;
-    }
-
-    /**
-     * @param string $eventContactEmail
-     */
-    public function setEventContactEmail(string $eventContactEmail): void
-    {
-        $this->eventContactEmail = $eventContactEmail;
-    }
-
-    /**
-     * @return string
-     */
-    public function getEventContactFacebook(): string
-    {
-        return $this->eventContactFacebook;
-    }
-
-    /**
-     * @param string $eventContactFacebook
-     */
-    public function setEventContactFacebook(string $eventContactFacebook): void
-    {
-        $this->eventContactFacebook = $eventContactFacebook;
-    }
-
-    /**
-     * @return string
-     */
-    public function getEventContactInstagram(): string
-    {
-        return $this->eventContactInstagram;
-    }
-
-    /**
-     * @param string $eventContactInstagram
-     */
-    public function setEventContactInstagram(string $eventContactInstagram): void
-    {
-        $this->eventContactInstagram = $eventContactInstagram;
-    }
-
-    /**
-     * @return string
-     */
-    public function getEventContactLinks(): string
-    {
-        return $this->eventContactLinks;
-    }
-
-    /**
-     * @param string $eventContactLinks
-     */
-    public function setEventContactLinks(string $eventContactLinks): void
-    {
-        $this->eventContactLinks = $eventContactLinks;
-    }
-
-    /**
-     * @return string
-     */
-    public function getEventContactLogo(): string
-    {
-        return $this->eventContactLogo;
-    }
-
-    /**
-     * @param string $eventContactLogo
-     */
-    public function setEventContactLogo(string $eventContactLogo): void
-    {
-        $this->eventContactLogo = $eventContactLogo;
-    }
-
-    /**
-     * @return string
-     */
-    public function getEventContactName(): string
-    {
-        return $this->eventContactName;
-    }
-
-    /**
-     * @param string $eventContactName
-     */
-    public function setEventContactName(string $eventContactName): void
-    {
-        $this->eventContactName = $eventContactName;
-    }
-
-    /**
-     * @return string
-     */
-    public function getEventContactTwitterHandle(): string
-    {
-        return $this->eventContactTwitterHandle;
-    }
-
-    /**
-     * @param string $eventContactTwitterHandle
-     */
-    public function setEventContactTwitterHandle(string $eventContactTwitterHandle): void
-    {
-        $this->eventContactTwitterHandle = $eventContactTwitterHandle;
     }
 
     /**
---
+++ src/Ventari/Domain/Model/Traits/ContactTrait.php
@@ -0,0 +1,160 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tollwerk
+ * Date: 02.08.2018
+ * Time: 11:00
+ */
+
+namespace Tollwerk\Ventari\Domain\Model\Traits;
+
+
+trait ContactTrait
+{
+    /**
+     * @var string
+     */
+    protected $eventContactEmail;
+
+    /**
+     * @var string
+     */
+    protected $eventContactFacebook;
+
+    /**
+     * @var string
+     */
+    protected $eventContactInstagram;
+
+    /**
+     * @var string
+     */
+    protected $eventContactLinks;
+
+    /**
+     * @var string
+     */
+    protected $eventContactLogo;
+
+    /**
+     * @var string
+     */
+    protected $eventContactName;
+
+    /**
+     * @var string
+     */
+    protected $eventContactTwitterHandle;
+
+    /**
+     * @return string
+     */
+    public function getEventContactEmail(): string
+    {
+        return $this->eventContactEmail;
+    }
+
+    /**
+     * @param string $eventContactEmail
+     */
+    public function setEventContactEmail(string $eventContactEmail): void
+    {
+        $this->eventContactEmail = $eventContactEmail;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEventContactFacebook(): string
+    {
+        return $this->eventContactFacebook;
+    }
+
+    /**
+     * @param string $eventContactFacebook
+     */
+    public function setEventContactFacebook(string $eventContactFacebook): void
+    {
+        $this->eventContactFacebook = $eventContactFacebook;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEventContactInstagram(): string
+    {
+        return $this->eventContactInstagram;
+    }
+
+    /**
+     * @param string $eventContactInstagram
+     */
+    public function setEventContactInstagram(string $eventContactInstagram): void
+    {
+        $this->eventContactInstagram = $eventContactInstagram;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEventContactLinks(): string
+    {
+        return $this->eventContactLinks;
+    }
+
+    /**
+     * @param string $eventContactLinks
+     */
+    public function setEventContactLinks(string $eventContactLinks): void
+    {
+        $this->eventContactLinks = $eventContactLinks;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEventContactLogo(): string
+    {
+        return $this->eventContactLogo;
+    }
+
+    /**
+     * @param string $eventContactLogo
+     */
+    public function setEventContactLogo(string $eventContactLogo): void
+    {
+        $this->eventContactLogo = $eventContactLogo;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEventContactName(): string
+    {
+        return $this->eventContactName;
+    }
+
+    /**
+     * @param string $eventContactName
+     */
+    public function setEventContactName(string $eventContactName): void
+    {
+        $this->eventContactName = $eventContactName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEventContactTwitterHandle(): string
+    {
+        return $this->eventContactTwitterHandle;
+    }
+
+    /**
+     * @param string $eventContactTwitterHandle
+     */
+    public function setEventContactTwitterHandle(string $eventContactTwitterHandle): void
+    {
+        $this->eventContactTwitterHandle = $eventContactTwitterHandle;
+    }
+}
