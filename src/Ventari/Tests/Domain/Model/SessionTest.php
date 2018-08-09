a58ee77782644499ad0392a8b04079864c3a62bf
cowglow <cowglow@gmail.com>
2018-08-09 19:54:22 +0200

    Session Class Test for Coverage

--- src/Ventari/Tests/Domain/Model/SessionTest.php
+++ src/Ventari/Tests/Domain/Model/SessionTest.php
@@ -23,15 +23,53 @@ class SessionTest extends AbstractTestBase
         $this->sessionApi = $factory->accessSessionApi();
     }
 
-    public function testSessionId(): void
+    /**
+     * Test Class Properties
+     *
+     * @param $input
+     *
+     * @dataProvider jsonInputProvider
+     */
+    public function testSessionId($input): void
     {
         $testClass = new Session();
-        foreach ($this->sessionApi as $property) {
+        $factory = new SessionFactory();
+
+        foreach ($input as $key => $value){
+            $property = (isset($this->sessionApi[$key])) ? $this->sessionApi[$key] : $this->sessionApi['sessionName'];
             $this->assertClassHasAttribute($property, Session::class);
+
             $setter = 'set'.ucfirst($property);
-            $getter = 'get'.ucfirst($property);
             $this->assertThat(method_exists($testClass, $setter), $this->equalTo(true));
+
+            $refinedValue = $factory->accessRefineValue($key, $value);
+            $testClass->$setter($refinedValue);
+            $getter = 'get'.ucfirst($property);
             $this->assertThat(method_exists($testClass, $getter), $this->equalTo(true));
+            $this->assertEquals($refinedValue, $testClass->$getter());
         }
+    }
+
+    public function jsonInputProvider(): array
+    {
+        return [
+            [
+                array(
+                    'sessionRemark'        => "",
+                    'rowNum'               => 2,
+                    'sessionId'            => 535,
+                    'sessionCategoryColor' => "#EEE8AA",
+                    'sessionName'          => "Musterevent NUEWW",
+                    'eventId'              => 1802,
+                    'sessionCategoryId'    => 1,
+                    'sessionCategoryName'  => "Programmpunkt",
+                    'sessionLineName'      => "Schiene 1",
+                    'sessionEnd'           => "October, 12 2018 17:30:00",
+                    'sessionLineId'        => 2,
+                    'sessionRoom'          => "",
+                    'sessionStart'         => "October, 12 2018 09:00:00"
+                )
+            ]
+        ];
     }
 }
