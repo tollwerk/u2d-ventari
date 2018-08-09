accbd627a1c4b3fef996503071d4448f37969d4e
cowglow <cowglow@gmail.com>
2018-08-09 16:46:18 +0200

    Update LocationTest for Coverage

--- src/Ventari/Tests/Domain/Model/EventTest.php
+++ src/Ventari/Tests/Domain/Model/EventTest.php
@@ -19,20 +19,79 @@ class EventTest extends AbstractTestBase
         $this->eventApi = $factory->accessEventApi();
     }
 
-    public function testClass(): void
+    /**
+     * Test Class Properties
+     *
+     * @param $input
+     *
+     * @dataProvider jsonInputProvider
+     */
+    public function testClass($input): void
     {
         $testClass = new Event();
+        $factory   = new EventFactory();
 
-        foreach ($this->eventApi as $property) {
+        foreach ($input as $key => $value) {
+            $property = (isset($this->eventApi[$key])) ? $this->eventApi[$key] : $this->eventApi['event_city'];
             $this->assertClassHasAttribute($property, Event::class);
+
             $setter = 'set'.ucfirst($property);
             $this->assertThat(method_exists($testClass, $setter), $this->equalTo(true));
-        }
 
-        foreach ($this->eventApi as $property) {
-            $this->assertClassHasAttribute($property, Event::class);
+            $refinedValue = $factory->accessRefineValue($key, $value);
+            $testClass->$setter($refinedValue);
             $getter = ($property !== 'hidden' && $property !== 'chargeable') ? 'get'.ucfirst($property) : 'is'.ucfirst($property);
             $this->assertThat(method_exists($testClass, $getter), $this->equalTo(true));
+            $this->assertEquals($refinedValue, $testClass->$getter());
         }
+    }
+
+    public function jsonInputProvider(): array
+    {
+        return [
+            [
+                array(
+                    'event_city'                   => "Nürnberg",
+                    'event_end_date'               => "2018-09-01",
+                    'active'                       => true,
+                    'event_start_time'             => "",
+                    'event_presentation_language'  => "",
+                    'event_contact_facebook'       => "",
+                    'event_targetgroup'            => "",
+                    'frontendLink'                 => "https://events.nueww.de/tms/frontend/frontend.cfm?l=1000",
+                    'event_level'                  => "",
+                    'event_tracks'                 => "",
+                    'event_max_participants'       => "",
+                    'event_twitter_handle'         => "",
+                    'event_contact_logo'           => "",
+                    'event_cost_description'       => "",
+                    'event_contact_instagram'      => "",
+                    'event_description_long'       => "",
+                    'event_endtime'                => "",
+                    'event_image'                  => "",
+                    'event_status'                 => "",
+                    'id'                           => 1000,
+                    'event_livestream'             => "",
+                    'event_other_tags'             => "",
+                    'event_xing_event'             => "",
+                    'event_category'               => "",
+                    'event_description'            => "Test",
+                    'event_cost'                   => "",
+                    'event_name'                   => "Eventanmeldung NUEWW 2018",
+                    'event_ticket_url'             => "",
+                    'event_chargeable'             => "",
+                    'event_typ'                    => "",
+                    'event_contact_name'           => "",
+                    'event_start_date'             => "2018-03-01",
+                    'event_contact_twitter_handle' => "",
+                    'event_contact_links'          => "",
+                    'event_facebook_event'         => "",
+                    'event_livestream_code'        => "",
+                    'event_contact_email'          => "",
+                    'event_id'                     => 1000,
+                    'event_website'                => ""
+                )
+            ]
+        ];
     }
 }
--- src/Ventari/Tests/Domain/Model/LocationTest.php
+++ src/Ventari/Tests/Domain/Model/LocationTest.php
@@ -23,19 +23,53 @@ class LocationTest extends AbstractTestBase
         $this->locationApi = $factory->accessLocationApi();
     }
 
-    public function testClass(): void
+    /**
+     * Test Class Properties
+     *
+     * @param $input
+     *
+     * @dataProvider jsonInputProvider
+     */
+    public function testClass($input): void
     {
         $testClass = new Location();
-        foreach ($this->locationApi as $property) {
+        $factory   = new LocationFactory();
+
+        foreach ($input as $key => $value){
+            $property = (isset($this->locationApi[$key])) ? $this->locationApi[$key] : $this->locationApi['hotelId'];
             $this->assertClassHasAttribute($property, Location::class);
+
             $setter = 'set'.ucfirst($property);
             $this->assertThat(method_exists($testClass, $setter), $this->equalTo(true));
-        }
 
-        foreach ($this->locationApi as $property) {
-            $this->assertClassHasAttribute($property, Location::class);
+            $refinedValue = $factory->accessRefineValue($key, $value);
+            $testClass->$setter($refinedValue);
             $getter = 'get'.ucfirst($property);
             $this->assertThat(method_exists($testClass, $getter), $this->equalTo(true));
+            $this->assertEquals($refinedValue, $testClass->$getter());
         }
+    }
+
+    public function jsonInputProvider(): array
+    {
+        return [
+            [
+                array(
+                    'hotelId'        => 3,
+                    'hotelTelephone' => "091123759913",
+                    'rowNum'         => 1,
+                    'hotelZip'       => 90489,
+                    'hotelName'      => "up2date solutions GmbH",
+                    'longitude'      => 11.087469,
+                    'eventId'        => 1801,
+                    'hotelCity'      => "Nürnberg",
+                    'hotelFax'       => "091123759913",
+                    'hotelAddress'   => "Prinzregentenufer 3",
+                    'companyId'      => 1,
+                    'latitude'       => 49.45334,
+                    'hotelEmail'     => "info@up2date-solutions.de"
+                )
+            ]
+        ];
     }
 }
